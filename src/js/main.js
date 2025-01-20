85704d83976df3bb8d7e51e10a78a09e7cca877a
ManGuevara <mguevaradelacruz@gmail.com>
2025-01-19 21:47:24 -0500

    corrigiendo importacion

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 
-import ProductListing from "./ProductList.mjs";
+import ProductListing from "./productList.mjs";
 
 const dataSource = new ProductData("tents");
 console.log(dataSource);
