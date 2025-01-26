2f9c6b364f72967f992eccff6c65f7159235bc83
ManGuevara <mguevaradelacruz@gmail.com>
2025-01-26 13:00:43 -0500

    finalizando el header y el footer dinamicos incluyendo el carrito

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,11 @@ import ProductListing from "./productList.mjs";
 import { loadHeaderFooter } from './utils.mjs'; 
 
 const dataSource = new ProductData("tents");
-console.log(dataSource);
+// console.log(dataSource);
 
-dataSource.getData().then(products => {
-    console.log(products)
-});
+// dataSource.getData().then(products => {
+    // console.log(products)
+// });
 const listElement = document.getElementById("listElement")
 
 const lista = new ProductListing("tents", dataSource, listElement);
